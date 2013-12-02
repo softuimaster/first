39cf892023ba475ef32f3ab1fd86a53773fbe1ee
softuimaster <softuimaster@gmail.com>
2013-12-02 14:25:05 +0900

    Update first.js

--- first.js
+++ first.js
@@ -1,1 +1,11 @@
 function a(){console.log(arguments.callee)}
+
+
+(funciton() {
+  var arr = [];
+  for(var i=0 ; i < 10000 ; i++) {
+    console.log(i);
+    arr.push(i+i+i);
+  }
+  return arr;
+})();
